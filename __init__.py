baf1dc49397bc412d18f814a3b10ecf8fd59b4d1
Abhijeet Singh <138706063+Abhijeet11263@users.noreply.github.com>
2023-07-13 12:57:56 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,13 +1,50 @@
-from typing import List, Optional
+from django.contrib.admin.decorators import action, display, register
+from django.contrib.admin.filters import (
+    AllValuesFieldListFilter,
+    BooleanFieldListFilter,
+    ChoicesFieldListFilter,
+    DateFieldListFilter,
+    EmptyFieldListFilter,
+    FieldListFilter,
+    ListFilter,
+    RelatedFieldListFilter,
+    RelatedOnlyFieldListFilter,
+    SimpleListFilter,
+)
+from django.contrib.admin.options import (
+    HORIZONTAL,
+    VERTICAL,
+    ModelAdmin,
+    StackedInline,
+    TabularInline,
+)
+from django.contrib.admin.sites import AdminSite, site
+from django.utils.module_loading import autodiscover_modules
 
-__version__ = "22.3.1"
+__all__ = [
+    "action",
+    "display",
+    "register",
+    "ModelAdmin",
+    "HORIZONTAL",
+    "VERTICAL",
+    "StackedInline",
+    "TabularInline",
+    "AdminSite",
+    "site",
+    "ListFilter",
+    "SimpleListFilter",
+    "FieldListFilter",
+    "BooleanFieldListFilter",
+    "RelatedFieldListFilter",
+    "ChoicesFieldListFilter",
+    "DateFieldListFilter",
+    "AllValuesFieldListFilter",
+    "EmptyFieldListFilter",
+    "RelatedOnlyFieldListFilter",
+    "autodiscover",
+]
 
 
-def main(args: Optional[List[str]] = None) -> int:
-    """This is an internal API only meant for use by pip's own console scripts.
-
-    For additional details, see https://github.com/pypa/pip/issues/7498.
-    """
-    from pip._internal.utils.entrypoints import _wrapper
-
-    return _wrapper(args)
+def autodiscover():
+    autodiscover_modules("admin", register_to=site)
