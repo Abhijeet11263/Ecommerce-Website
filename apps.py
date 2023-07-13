0cff83fc3d71f9b9e7c13a82277f47ea1b449ef2
Abhijeet Singh <138706063+Abhijeet11263@users.noreply.github.com>
2023-07-13 21:49:06 +0530

    Add files via upload

--- apps.py
+++ apps.py
@@ -1,17 +1,13 @@
 from django.apps import AppConfig
-from django.contrib.sites.checks import check_site_id
+from django.contrib.staticfiles.checks import check_finders
 from django.core import checks
-from django.db.models.signals import post_migrate
 from django.utils.translation import gettext_lazy as _
 
-from .management import create_default_site
 
-
-class SitesConfig(AppConfig):
-    default_auto_field = "django.db.models.AutoField"
-    name = "django.contrib.sites"
-    verbose_name = _("Sites")
+class StaticFilesConfig(AppConfig):
+    name = "django.contrib.staticfiles"
+    verbose_name = _("Static Files")
+    ignore_patterns = ["CVS", ".*", "*~"]
 
     def ready(self):
-        post_migrate.connect(create_default_site, sender=self)
-        checks.register(check_site_id, checks.Tags.sites)
+        checks.register(check_finders, checks.Tags.staticfiles)
--- checks.py
+++ checks.py
@@ -1,12 +1,14 @@
-from django.conf import settings
-from django.core.checks import Error
+from django.contrib.staticfiles.finders import get_finders
 
 
-def check_site_id(app_configs, **kwargs):
-    if hasattr(settings, "SITE_ID") and not isinstance(
-        settings.SITE_ID, (type(None), int)
-    ):
-        return [
-            Error("The SITE_ID setting must be an integer", id="sites.E101"),
-        ]
-    return []
+def check_finders(app_configs=None, **kwargs):
+    """Check all registered staticfiles finders."""
+    errors = []
+    for finder in get_finders():
+        try:
+            finder_errors = finder.check()
+        except NotImplementedError:
+            pass
+        else:
+            errors.extend(finder_errors)
+    return errors
